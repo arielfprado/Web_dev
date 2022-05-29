beb65888b2ba63838483c4528ffe2374519e260d
arielfprado <arielfprado96@gmail.com>
2022-05-29 19:04:14 -0300

    memory game enhancement with gifs, header and play again option

--- Memory_game/script.js
+++ Memory_game/script.js
@@ -2,6 +2,7 @@ const cards = document.querySelectorAll(".card");
 let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false; /*to prevent from clicking another card while they are unflipping*/
+let matches = 0;
 
 
 function flipCard() {
@@ -25,8 +26,9 @@ function checkForMatch() {
     if(firstCard.dataset.card === secondCard.dataset.card) {
         firstCard.classList.add('match');
         secondCard.classList.add('match');
-        
+        matches +=2;
         disableCards();
+        checkEndGame();
         return;
     }
 
@@ -56,16 +58,39 @@ function resetBoard() {
     [firstCard, secondCard] = [null, null];
 }
 
-/*immediatle invoked function - to shuffle the cards i the beggining of each session*/
-(function shuffle() {
+/*immediatle invoked function - to shuffle the cards in the beggining of each session*/
+function shuffle() {
     cards.forEach((card) => {
         let randomPosition = Math.floor(Math.random()*12);
         card.style.order = randomPosition;
     })
-})();
+}
+shuffle();
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 })
 
-
+function checkEndGame() {
+   if(matches>=12) {
+       let game = prompt("Você gostaria de jogar novamente? (S ou N): ");
+       
+       if(game === 'S') {
+            resetBoard();
+            cards.forEach((card) => {
+            card.addEventListener('click', flipCard);
+            card.classList.remove('flip');
+            card.classList.remove('match');
+            })
+            setTimeout(() => {
+                shuffle();
+            },1000);
+            matches=0;
+       } else if(game === 'N') {
+            alert('Até mais!');
+       } else {
+            alert("Opção inválida, responda S ou N.");
+            checkEndGame();
+       }    
+   }
+}
